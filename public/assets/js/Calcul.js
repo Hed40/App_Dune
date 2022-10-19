0d3451deeff264a44f49920f2630798e484340ce
Hed40 <90209761+Hed40@users.noreply.github.com>
2022-10-19 07:12:30 +0200

    Front - ProgressBar v.1

--- public/assets/js/Calcul.js
+++ public/assets/js/Calcul.js
@@ -1,15 +1,15 @@
-const array =[calc_2()]
+const array =[calc_1(), calc_2()]
 
 
 function calc_1(result_1) {
-    for (let h = 0; h < 1608; h++) {
+    for (let h = 0; h < 10; h++) {
    result_1 = console.log('.progress-circle[data-value="' + h + '"]');
     }
   }
   
   
   function calc_2(result_2) {
-    for (let i = 0; i <= 361; i = i + 0.2245024875621891) {
+    for (let i = 0; i <= 360; i = i + 36) {
       result_2 = console.log(".progress-barre {transform: rotate(" + i + "deg)}");
     }
   }
